9372f4ef28944d1878005921c29aa75e9aaa4fc6
Antony <tonyxii@gmail.com>
2023-12-12 09:56:51 +0100

    first commit with files

---
+++ ParserToDatabaseToExcel/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.OpenFileToParseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.info = New System.Windows.Forms.Label()
+        Me.SendDB = New System.Windows.Forms.Button()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenFileToParseToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'OpenFileToParseToolStripMenuItem
+        '
+        Me.OpenFileToParseToolStripMenuItem.Name = "OpenFileToParseToolStripMenuItem"
+        Me.OpenFileToParseToolStripMenuItem.Size = New System.Drawing.Size(62, 24)
+        Me.OpenFileToParseToolStripMenuItem.Text = "Parser"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(27, 69)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(365, 257)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(411, 69)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(365, 257)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(24, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Json"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(411, 47)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Clean Json"
+        '
+        'info
+        '
+        Me.info.AutoSize = True
+        Me.info.Location = New System.Drawing.Point(139, 363)
+        Me.info.Name = "info"
+        Me.info.Size = New System.Drawing.Size(37, 16)
+        Me.info.TabIndex = 5
+        Me.info.Text = "info : "
+        '
+        'SendDB
+        '
+        Me.SendDB.Location = New System.Drawing.Point(49, 360)
+        Me.SendDB.Name = "SendDB"
+        Me.SendDB.Size = New System.Drawing.Size(75, 23)
+        Me.SendDB.TabIndex = 6
+        Me.SendDB.Text = "SendDB"
+        Me.SendDB.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SendDB)
+        Me.Controls.Add(Me.info)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents OpenFileToParseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents info As Label
+    Friend WithEvents SendDB As Button
+End Class
---
+++ ParserToDatabaseToExcel/Form1.vb
@@ -0,0 +1,169 @@
+﻿Imports System.Diagnostics.Eventing
+Imports System.IO
+Imports System.Reflection.Emit
+Imports System.Runtime.InteropServices
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports Microsoft.Office.Interop.Excel
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+Imports Oracle.DataAccess.Client
+Imports Oracle.DataAccess.Types
+
+
+Public Class Form1
+
+    Dim userId = "TESTDOCKER"
+    Dim password = "TESTDOCKER"
+    Dim dataSource = "localhost:49161/xe"
+
+#If False Then
+
+    Dim conn As New OracleConnection
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Try
+            conn.ConnectionString = "User Id=" + userId + ";Password=" + password + ";Data Source=" + dataSource + ";"
+            conn.Open()
+            TextBox1.Text = "Connected"
+        Catch ex As Exception
+            TextBox1.Text = "Error: " & ex.Message
+        End Try
+
+        Dim cmd As New OracleCommand("Select CITY_NAME from CITY", conn)
+        Dim answer As OracleDataReader = cmd.ExecuteReader()
+
+        While answer.Read()
+
+            Dim cityName As String = answer.GetString(0)
+            TextBox1.AppendText(cityName)
+
+        End While
+        conn.dispose()
+    End Sub
+
+
+    Dim excelApp As New Application()
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        ' Specify the path to the Excel file (XLSX) you want to check
+        Dim filePath As String = "D:\VisualBasic\databaseConnectVB\xlsx\test.xlsx"
+        ' Create a new Excel Application and open a workbook
+
+
+        Label1.Text = ""
+
+
+        ' Try to open the workbook. If it's already open, it will throw an exception.
+        Dim workbook As Workbook = excelApp.Workbooks.Open(filePath)
+        Try
+            'workbook = excelApp.Workbooks.Open(filePath)
+
+            ' Access a specific worksheet, e.g., "Sheet1"
+            Dim worksheet As Worksheet = workbook.Sheets("Sheet1")
+
+            ' Find the next available row in column 1 (A)
+            Dim lastRow As Integer = worksheet.Cells(worksheet.Rows.Count, 1).End(XlDirection.xlUp).Row
+
+            ' Write data to the next available row in column 1 (A)
+            worksheet.Cells(lastRow + 1, 1).Value = TextBox2.Text
+
+            Label1.Text = "Added " + TextBox2.Text + " to the excel file in the cell A" + lastRow.ToString
+
+            ' Print a message to the console
+            Console.WriteLine(lastRow)
+
+            ' Save the workbook with the changes
+            workbook.Save()
+
+            ' Close and release Excel objects
+            workbook.Close()
+            'excelApp.Quit()
+            Marshal.ReleaseComObject(workbook)
+            'Marshal.ReleaseComObject(excelApp)
+        Catch ex As Exception
+            ' The workbook is already open or couldn't be opened.
+            Console.WriteLine("Excel workbook is already open or cannot be opened.")
+            Return
+        End Try
+
+
+
+        ' Write data to a specific cell, e.g., cell A1
+        ' worksheet.Cells(1, 1).Value = TextBox2.Text
+
+        ''''''''''''''''''''''''''''''''''''''''''''''
+        ''''''''''''''''''''''''''''''''''''''''''''''''''
+
+    End Sub
+
+#End If
+
+    Private Sub OpenFileToParseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenFileToParseToolStripMenuItem.Click
+        ' Create an instance of OpenFileDialog
+        Dim openFileDialog As New OpenFileDialog()
+
+        ' Set properties of the OpenFileDialog
+        openFileDialog.Title = "Select a JSON File"
+        openFileDialog.Filter = "JSON Files|*.json|All Files|*.*" ' Specify file filters for JSON
+        openFileDialog.InitialDirectory = Environment.CurrentDirectory
+        ' Set the initial directory
+
+        ' Show the OpenFileDialog and check if the user clicked OK
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            ' Get the selected file name
+            Dim selectedFileName As String = openFileDialog.FileName
+            Dim fileContent As String = File.ReadAllText(selectedFileName)
+
+            ProcessFileContent(fileContent)
+        End If
+    End Sub
+
+    Private Sub ProcessFileContent(jsonString As String)
+        TextBox1.Text = jsonString
+
+        ' Deserialize the JSON string
+        Dim jsonArray As JArray = JsonConvert.DeserializeObject(jsonString)
+
+        ' Convert the JArray to a string and remove square brackets and curly braces
+        Dim cleanJsonString As String = jsonArray.ToString().Replace("[", "").Replace("]", "").Replace("{", "").Replace("}", "")
+        TextBox2.Text = cleanJsonString
+    End Sub
+
+
+    Dim conn As New OracleConnection
+
+    Private Sub SendDB_Click(sender As Object, e As EventArgs) Handles SendDB.Click
+        Try
+            conn.ConnectionString = "User Id=" + userId + ";Password=" + password + ";Data Source=" + dataSource + ";"
+            conn.Open()
+            info.Text = "info : Connected"
+        Catch ex As Exception
+            info.Text = "info : Error: " & ex.Message
+            Return
+        End Try
+
+        ' Assuming you have a function to create the JSON string from your data
+        Dim jsonString As String = TextBox1.Text
+
+        ' Call the stored procedure with the JSON parameter
+        Try
+            Using cmd As New OracleCommand("InsertJsonData", conn)
+                cmd.CommandType = CommandType.StoredProcedure
+                cmd.Parameters.Add("p_json_string", OracleDbType.Clob).Value = jsonString
+                cmd.ExecuteNonQuery()
+            End Using
+
+            info.Text = "info : JSON data sent successfully."
+        Catch ex As Exception
+            info.Text = "info : Error: " & ex.Message
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
